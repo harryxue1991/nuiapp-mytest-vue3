d820e612284b508aef3faa6d9d8e28e3d65bde52
薛辛超 <xinchao.xue@bioon.com>
2022-03-18 16:26:23 +0800

    feat: 首次添加

---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup>
+  import { onLoad, onShow } from '@dcloudio/uni-app'
+  import { ref } from "vue"
+  onLoad((options) => {
+    console.log(options)
+    console.log('hello')
+  })
+  onShow(() => {
+    console.log('onshow')
+  })
+
+  const title = ref('hello world')
+</script>
+
+<style scoped lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: $uni-text-color;
+}
+</style>
